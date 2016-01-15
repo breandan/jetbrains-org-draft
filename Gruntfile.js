9b05add7876cf34f4003a3568728364f3855f036
breandan considine <breandan.considine@gmail.com>
2016-01-14 18:15:30 -0800

    Built C:\Users\breandan\jetbrains-org from commit 7000a0e on branch master

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        exec: {
+            jekyll: {
+                cmd: 'jekyll build --trace'
+            }
+        },
+        buildcontrol: {
+            options: {
+                dir: '_site',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages: {
+                options: {
+                    remote: 'git@github.com:tscholak/tscholak.github.io.git',
+                    branch: 'gh-pages'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-build-control');
+
+    grunt.registerTask('build', ['exec:jekyll']);
+    grunt.registerTask('deploy', ['build', 'buildcontrol:pages']);
+    grunt.registerTask('default', ['deploy']);
+};
